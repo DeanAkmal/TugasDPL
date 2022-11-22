ac5a0796768d5600a90d3e550a90f6d13c9e349d
DeanAkmal <118705266+DeanAkmal@users.noreply.github.com>
2022-11-22 13:36:00 +0700

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,21 @@
+//var i = 5;
+
+//for (i = 0; i < 10; i++ )
+
+//console.log("Nilai sekarang " + i)
+var i = bil;
+for (i = 0; i <= 10; i++ )
+if (i % 2 == 0 ){
+    console.log(i)};
+
+var input = require('readline-sync');
+var bil = input.question('Input bilangan : ');
+
+if (bil > 10){
+    console.log("BIlangan melebihi batas");
+}else{
+
+for (var i = 0; i <= bil; i++)    
+    if( i % 2 == 0) 
+    console.log(i);
+};
